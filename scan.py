1d57abb5a67cfa8b4b0b75ced41bb8f124bf536b
m202a-winter19-lofi <m202a.winter19.lofi@gmail.com>
2019-03-17 07:56:21 +0000

    spruced up scan.py (was read.py) and cherrypy/project.py, should work in theory but not yet tested

--- scan.py
+++ scan.py
@@ -7,21 +7,25 @@ class BTEventHandler(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
         self.device_macs = []
+        self.output_file = open("/home/pi/cherrypy/va.txt", "w")
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         """ Adv data """
         if isNewDev:
-            print "Found new device:", dev.addr, dev.getScanData()
+            #print "Found new device:", dev.addr, dev.getScanData()
             self.device_macs.append(dev.addr)
 
         """ Scan response """
         if isNewData:
-            print "Received more data:", dev.addr, dev.getScanData()
+            #print "Received more data:", dev.addr, dev.getScanData()
+            pass
 
     def handleNotification(self, cHandle, data):
         """ Print value only if handle is 40, battery characteristic """
         if cHandle == 40:
-            print struct.unpack('B', data)
+            unpacked = struct.unpack('B', data)
+            print unpacked
+            f.write(unpacked[0]) # write data to file
 
 handler = BTEventHandler()
 
@@ -29,7 +33,9 @@ handler = BTEventHandler()
 scanner = Scanner().withDelegate(handler)
 
 """ Start scanning for (argument) seconds. While scanning, handleDiscovery will be called whenever a new device or new data is found"""
-devs = scanner.scan(30)
+scan_time = 30
+print "Scanning devices for " + str(scan_time) + " seconds"
+devs = scanner.scan(scan_time)
 
 """ After end of scan, print all device addresses heard """
 for mac in handler.device_macs:
@@ -37,6 +43,7 @@ for mac in handler.device_macs:
 
 """ Get Hexiwear address """
 hexi_addr = [dev for dev in devs if dev.getValueText(0x8) == 'HEXIWEAR'][0].addr 
+print "Found Hexiwear with MAC address " + str(hexi_addr)
 
 """ Create peripheral object with the delegate """
 hexi = Peripheral().withDelegate(handler)
@@ -52,7 +59,12 @@ battery_desc = battery.getDescriptors(forUUID=0x2902)[0]
 battery_desc.write(b"\x01", True)
 
 """ Infinite loop to receive notifications """
-print "Entering notification receiving loop"
+print "Connect successful. Entering notification receiving loop"
 while True:
-    hexi.waitForNotifications(1.0)
+    #hexi.waitForNotifications(1.0)
+    try:
+        hexi.waitForNotifications(1.0)
+    except:
+        print "No values received"
+
 
