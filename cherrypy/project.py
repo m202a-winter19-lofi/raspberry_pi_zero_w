6ce264fff20d85609b3d95b815c220a7d9408307
m202a-winter19-lofi <m202a.winter19.lofi@gmail.com>
2019-03-17 22:45:12 +0000

    fixed cherrypy/project.py , did not read file to open properly before

--- cherrypy/project.py
+++ cherrypy/project.py
@@ -3,9 +3,10 @@ import cherrypy
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        with open ('va.txt', 'r') as myfile:
+        with open ('/home/pi/cherrypy/va.txt', 'r') as myfile:
             value = myfile.read().replace('\n', '')
-        refresh = '<meta http-equiv="refresh" content="10">'
+        #refresh = '<meta http-equiv="refresh" content="10">'
+        print(value)
         return value
 
 if __name__ == '__main__':
